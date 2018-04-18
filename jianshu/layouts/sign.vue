74e013cd8a679f2a30bee901fac22b672c160acc
23sdsd1 <a66914903>
2018-04-18 20:00:51 +0800

    第5次修改：登录页面创建

---
+++ jianshu/layouts/sign.vue
@@ -0,0 +1,127 @@
+<template>
+  <div>
+    <nuxt/>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  // 修改当前页面的默认模板为 sign.vue
+
+  head: {
+    title: "登录-简书"
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+body {
+  background-color: #f1f1f1;
+}
+.sign {
+  height: 100%;
+  font-size: 14px;
+  text-align: center;
+}
+.sign .logo {
+  position: absolute;
+  top: 50px;
+  left: 50px;
+}
+.sign img {
+  width: 100px;
+}
+.sign .main {
+  width: 400px;
+  margin: 50px 0 0;
+  padding: 50px 50px 30px;
+  background-color: #fff;
+  border-radius: 4px;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.1);
+  display: inline-block;
+  vertical-align: middle;
+}
+.sign .main .title {
+  margin: 0 auto 30px;
+  color: #939393;
+  font-size: 18px;
+}
+.sign .main h4 a {
+  padding: 10px;
+  color: #969696;
+  font-weight: 400;
+}
+.sign .main h4 b {
+  font-weight: 700;
+  padding: 10px;
+  color: #969696;
+}
+.sign .main h4 a.active {
+  color: #ea6f5a;
+  border-bottom: 2px solid #ea6f5a;
+}
+.sign .main h4 a:hover {
+  border-bottom: 2px solid #ea6f5a;
+  color: #ea6f5a;
+}
+.sign .main form {
+  color: #969696;
+  margin: 0 0 30px;
+}
+.sign .main form .input-prepend {
+  position: relative;
+  width: 100%;
+}
+.sign .main form .help {
+  float: right;
+  margin: 20px 0px;
+  color: #969696;
+}
+.sign .main form .help:hover{
+    color: #333;
+}
+.sign .main form .input-prepend input {
+  width: 100%;
+  height: 50px;
+  padding: 4px 12px 4px 30px;
+  border: 1px solid #c8c8c8;
+  border-radius: 2px;
+  background-color: rgb(248, 248, 248);
+}
+.sign .main form .input-prepend:nth-of-type(1) input {
+  border-bottom: 0;
+  border-radius: 2px 2px 0 0;
+}
+.sign .main form .input-prepend:nth-of-type(2) input {
+  /* border-bottom: 0; */
+  border-radius: 0 0 2px 2px;
+}
+.sign .main form .input-prepend i {
+  position: absolute;
+  left: 12px;
+  top: 15px;
+  font-size: 20px;
+  color: #969696;
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+
+.sign .main form .remember {
+  float: left;
+  margin: 20px 0;
+}
+.sign .main form button {
+  width: 100%;
+  /* margin: 20px 0 0; */
+  padding: 9px 18px;
+  text-align: center;
+  color: #fff;
+  background: #3194d0;
+  border-radius: 25px;
+}
+.sign .main form button:hover {
+  background: #187cb7;
+}
+</style>
---
+++ jianshu/pages/sign-up.vue
@@ -0,0 +1,38 @@
+<template>
+  
+  <div>
+      <p>注册</p>
+  </div>
+</template>
+
+
+<script>
+      export default {
+        // 设置页面的标题以及描述，关键字等原信息
+         head:{
+             layout:'sign',
+             title:'注册-简书',
+             meta:[
+                 {charset:'utf-8'},
+                 {name:'keywords',content:'注册页面'},
+                 {name:'description',content:'注册简书'},
+                 {name:'author',content:'沈龙'}
+             ]
+         },
+          data () {
+            return {
+                name:"登录"
+            }
+        }
+    }
+
+
+
+</script>
+
+
+<style>
+
+
+
+</style>
